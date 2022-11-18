68e42f7160dbe3a7891cd137004e8960bcb21e29
hoando <hoando.hdd@gmail.com>
2022-11-17 23:35:17 -0700

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+import dao.StudentDAOImpl;
+import model.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import service.report.ReportService;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // The following code initializes this project using the Spring configuration in applicationContext.xml
+        // TODO Check src/main/resources/applicationContext.xml to see how it initialized the JdbcTemplate bean.
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Get the Spring bean that manages access to Database
+        // TODO Change the following line of code after you finish implementing StudentDAOImpl
+//        StudentDAOImpl studentDAO = (StudentDAOImpl) context.getBean("studentDAOImpl");
+        StudentDAOImpl studentDAO = context.getBean(StudentDAOImpl.class);
+
+        // Using the JdbcTemplate to query for a student in the database
+        Student student = studentDAO.getStudentById(12345);
+        //BeanPropertyRowMapper maps each field in a row of the query result to the student object.
+        //what if table columns and class data members have different names?
+
+        System.out.println("Using queryForObject method from JdbcTemplate to query studentID=12345: ");
+        if (student != null) {
+            System.out.println(student.toString());
+        }
+
+        // Using the JdbcTemplate to query for all students in the database
+        System.out.println("\n" +
+                "Using the query method from JdbcTemplate to query all students: ");
+        List allStudents = studentDAO.getAllStudents();
+        for (Object stud : allStudents) {
+            System.out.println(stud.toString());
+        }
+
+        System.out.println("\n-----Implementation of ReportService----- ");
+
+        ReportService reportService = (ReportService) context.getBean("reportService");
+        reportService.printReport(System.out);
+
+    }
+}
